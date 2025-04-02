0be9239d037fef8f72d514ff494a1ffc39ce4453
simonkaerts <kaerts.simon@gmail.com>
2025-04-02 13:22:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch((error) => {
+                console.log('Service Worker registration failed:', error);
+            });
+    });
+}
+
+
 // Fix Timer Button Event Listener
 document.getElementById("startButton").addEventListener("click", startTimer);
 
