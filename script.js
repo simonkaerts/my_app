7e1b0617d05028fa78e3a26962a38cf12814573a
simonkaerts <kaerts.simon@gmail.com>
2025-04-02 14:03:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')  // Ensure it's root-relative
+    navigator.serviceWorker.register('/my_app/service-worker.js')  // Ensure it's root-relative
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
